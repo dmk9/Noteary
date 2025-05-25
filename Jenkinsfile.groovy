ba5bfc012e2a58e524a63d4e8a3918c6c337d516
Faithy847 <ifychuks77@gmail.com>
2025-05-25 12:46:44 +0100

    Noteary

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -24,7 +24,7 @@ pipeline {
     stage('Build') {
       steps {
         echo "Building Docker images..."
-        bat 'docker-compose build'
+        bat 'docker-compose -f docker-compose.yaml build'
       }
     }
 
